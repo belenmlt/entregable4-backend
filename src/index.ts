30ef14aae43a6c73eb44e54bdbf8728ad5ae2c53
Belén MLT <belenmlt@hotmail.com>
2020-12-10 11:55:42 +0100

    mongoose.connect moved to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+import mongoose from "mongoose";
 import express from "express";
 import { authorsRoutes } from "./routes/authors.routes";
 import { booksRoutes } from "./routes/books.routes";
@@ -11,15 +11,18 @@ const app = express();
 // Puerto
 app.set("port", process.env.PORT);
 
-// Inicializar server
-app.listen(app.get("port"), () => {
-    console.log(`Server working on port ${app.get("port")}`);
-    
-});
-
 // Middlewares
 app.use(express.json());
 
 // Rutas
 app.use("/authors", authorsRoutes.router);
 app.use("/books", booksRoutes.router);
+
+mongoose.connect(`mongodb+srv://belenmlt:${process.env.MATLASPASS}@cluster0.xq5kj.mongodb.net/library?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    // Inicializar server
+    app.listen(app.get("port"), () => {
+        console.log(`Server working on port ${app.get("port")}`);    
+    });
+})
+.catch(e => console.log('Ha ocurrido un error:', e));
